25021e90db35939bd8ee3d7b536e9129e7df0416
Wisani Shilumani <wshilumani@gmail.com>
2021-04-16 18:04:26 +0200

    feature: remove config

--- firebase.ts
+++ firebase.ts
@@ -3,13 +3,6 @@ import firebase from "firebase";
 import "firebase/firestore";
 // import "firebase/functions";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCzFGsn5lAMSmf9Ze7fWWaZTmAsjPxQiqk",
-  authDomain: "wics-app-4bb56.firebaseapp.com",
-  projectId: "wics-app-4bb56",
-  storageBucket: "wics-app-4bb56.appspot.com",
-  messagingSenderId: "478302571260",
-  appId: "1:478302571260:web:76a16604a50fe7b9d8e579",
-};
+const firebaseConfig = {}; // add your firebase config here
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
